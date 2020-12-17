4a38e3e6b208b2a67206f92b9e4b56ba244dcaca
Mael FOSSO <fosso.mael.elvis@gmail.com>
2020-12-17 23:28:05 +0100

    project modified committed

---
+++ android/app/src/main/kotlin/cm/guitou/mobile/evaluation_des_radios_communautaires/MainActivity.kt
@@ -0,0 +1,12 @@
+package cm.guitou.mobile.evaluation_des_radios_communautaires
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
